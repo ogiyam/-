7b43522dd812f8a422b89801a837873c8ee1fe4d
ogiyam <miyuogym@gmail.com>
2021-09-28 13:35:05 +0000

    [Fix]インデント修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,22 +12,12 @@ class User < ApplicationRecord
 
 
   has_one_attached :image
-# 　validate :image_type
-
+ 
   def self.guest
     find_or_create_by(email: 'guest@example.com') do |user|
     user.name = "ゲスト"
     user.password = SecureRandom.urlsafe_base64
     end
   end
-
-  private
-
-  # def image_type
-  # if !image.blob.content_type.in?(%('image/jpeg image/png'))
-  #   image.purge
-  #   errors.add(:image, 'はJPEGまたはPNG形式を選択してアップロードしてください')
-  # end
-  # end
 
 end
